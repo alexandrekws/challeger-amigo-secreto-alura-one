e2a5a2dd99cd08eb399a26dc4f83626604a7c6f0
alexandrekws <alexandrekawanishi8@gmail.com>
2025-02-26 17:12:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeNomes = [];
+let amigoSecreto = sorteioAmigoSecreto();
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+    let validacaoNome = /\d/;
+    
+    if (listaDeNomes.includes(nomeAmigo)){
+        alert("Nome já adicionado a lista.");
+    } else if (nomeAmigo == "" || validacaoNome.test(nomeAmigo)) {
+        alert('Por favor, preencha um nome válido');
+    } else {
+        listaDeNomes.push(nomeAmigo);
+        exibirTextoNaTela('listaAmigos', listaDeNomes.map(nome => `<li>${nome}</li>`).join(''));
+        limparCampo();
+    }
+}
+
+function exibirTextoNaTela(id, texto) {
+    let campo = document.getElementById(id);
+    campo.innerHTML = texto;
+}
+
+function sortearAmigo() {
+    let quantidadeMaxima = listaDeNomes.length;
+    let sorteado = parseInt(Math.random() * quantidadeMaxima);
+
+    mensagemSorteio = `O amigo secreto sorteado é: ${listaDeNomes[sorteado]}.`;
+
+    exibirTextoNaTela('resultado', mensagemSorteio);
+    exibirTextoNaTela('listaAmigos', '');
+}
+
+function limparCampo() {
+    campo = document.querySelector('input');
+    campo.value = '';
+}
